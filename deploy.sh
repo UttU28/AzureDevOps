896de29b8a543105d12a4caa3915de1d59a069bc
UttU28 <utsavmaan28@gmail.com>
2024-05-20 16:30:09 -0500

    JSK

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ az group show --name DevOpsRG --query id -o tsv
 az ad sp create-for-rbac -n "spn-aca-azure-pipelines" --role Contributor --scope /subscriptions/fb03483b-eece-41f0-8fca-b16b9e75cb43
 
 # {
-#   "appId": "98b5470b-3433-41fe-ac3a-75efc00e45555560e",
-#   "displayName": "spn-aca-azure-pipeli55555nes",
-#   "password": "z9.8Q~SgvbuWe3NrxhsAEA5vmITNBkUrlSXFy55555cSQ",
-#   "tenant": "a8d3e12a-a4f9-47c3-b7bd-0f18f50c555554713"
+#   "appId": "98b5470b-3433-41fe-ac3a-75efc00e460e",
+#   "displayName": "spn-aca-azure-pipelines",
+#   "password": "z9.8Q~SgvbuWe3NrxhsAEA5vmITNBkUrlSXFycSQ",
+#   "tenant": "a8d3e12a-a4f9-47c3-b7bd-0f18f50c4713"
 # }
